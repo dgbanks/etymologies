638ad416bbd25b44b70ebcabd0919a90507994b2
David <davidgbanks91@gmail.com>
2018-01-23 16:20:32 -0800

    Install express, react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import SearchField from './search_field';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SearchField />, document.getElementById('root'));
 registerServiceWorker();
---
+++ notes.md
@@ -0,0 +1,8 @@
+### notes for MERN App
+
+#### Server Side Architecture
+* Node: JS runtime used to execute code outside browser
+* Express: library that uses Node runtime, makes dealing with HTTP traffic easier
+  * looks at the requests and decides what code will handle the response
+  
+
