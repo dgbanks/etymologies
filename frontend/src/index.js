60ede5cea27ed1eb0385d5a6a3fa3087387ac841
David <davidgbanks91@gmail.com>
2018-01-24 19:33:31 -0800

    Remove previously initialized react app

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import SearchField from './search_field';
-
-ReactDOM.render(<SearchField />, document.getElementById('root'));
-registerServiceWorker();
