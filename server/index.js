403c8f551017e0b1519649617e029873b121d964
David <davidgbanks91@gmail.com>
2018-01-24 19:31:17 -0800

    Make changes up to MERN tutorial lesson 22

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express'); // node does not have support for ES6
-const app = express(); 
+const app = express();
 // inside a singe node.js project, there can be many express applications running
 // calling express like a function creates a new app
 
@@ -8,5 +8,10 @@ app.get('/', (request, response) => {
   response.send({hi: 'there' });
 });
 
-app.listen(5000); // tells node which port to listen to 
+const PORT = process.env.PORT || 5000;
+// heroku can inject environment variables set in the node runtime
+// find the port in the underying environment
 
+app.listen(PORT); // tells node which port to listen to
+// heroku will tell us what port our app will use, so we must listen to the right one
+// specify the node version we are using
